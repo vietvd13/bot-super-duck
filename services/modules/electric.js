f6544fd18b5211ee5405b337d1b138ad733ee509
Duck <vuducviet0131@gmail.com>
2023-06-29 00:59:27 +0700

    Format code services

---
+++ services/modules/electric.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const config = require("../../config/index.json");
+const { getDateTomorrow } = require('../../helpers/index.js');
+
+const getElectricCutSchedule = async () => {
+    try {
+        const URL = config.ENV_URL_API;
+        const dateTomorrow = getDateTomorrow();
+        const listLocation = config.ENV_LIST_LOCATION;
+
+        const len = listLocation.length;
+        let idx = 0;
+
+        const result = [];
+
+        while (idx < len) {
+            const { code } = listLocation[idx];
+
+            const BODY = {
+                ngayBatDau: dateTomorrow,
+                ngayKetThuc: dateTomorrow,
+                maDViQly: code
+            }
+
+            const { data } = await axios.post(URL, BODY);
+
+            if (data) {
+                const lichCatdienToList = data.data.lichCatdienToList || [];
+
+                result.push({
+                    name: listLocation[idx].name,
+                    data: lichCatdienToList
+                });
+            }
+
+            idx++;
+        }
+
+        return result;
+    } catch (error) {
+        console.log("[ERROR] CALL API GET ELECTRIC");
+        console.error(error);
+
+        return null;
+    }
+};
+
+module.exports = {
+  getElectricCutSchedule
+}
